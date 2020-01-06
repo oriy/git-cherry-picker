08a3860c3bc254b82dc5036b4d492d6eaec1f215
Ori Yechieli <ori.yechieli@locusview.com>
2020-01-06 21:55:59 +0200

    cherry-pick unique docker container name

--- docker_exec.sh
+++ docker_exec.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
 function _docker_exec() {
+
+    containerName="cherry${JOB_NAME}${BUILD_NUMBER}"
+
     # remove any existing cherry container
-    docker rm --force cherry >/dev/null
+    docker rm --force "${containerName}" >/dev/null
+
+    docker pull nortecview/cherry_picker:latest
 
     # run cherry container in background
-    docker run --name cherry --detach --tty nortecview/cherry_picker:latest >/dev/null
+    docker run --name "${containerName}" --detach --tty nortecview/cherry_picker:latest >/dev/null
 
     echo "running on docker: '$1' arguments: '${@:2}'"
-    docker exec --interactive cherry /bin/bash -c "$1 $(echo "${@:2}" | sed 's/ /\\ /g')"
+    docker exec --interactive "${containerName}" /bin/bash -c "$1 $(echo "${@:2}" | sed 's/ /\\ /g')"
+
+    docker rm --force "${containerName}" >/dev/null
 }
